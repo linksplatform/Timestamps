8e9e75d2bd7555a236e68ee0d30d63ef62918346
Stepan Ilyin <norwend@tuta.io>
2021-05-04 01:27:30 +0300

    first rewrite try

--- cpp/Platform.Timestamps/Timestamp.h
+++ cpp/Platform.Timestamps/Timestamp.h
@@ -1,25 +1,35 @@
-﻿namespace Platform::Timestamps
+﻿// Since I'm working only with this repo, my compiler doesn't really like the absence of std in this file, I'm not sure
+// whether uint64_t is gonna work without "std::", so I'm gonna remove them for now
+
+namespace Platform::Timestamps
 {
-    struct Timestamp : public IEquatable<Timestamp>
+    class Timestamp : public IEquatable<Timestamp>
     {
-        public: inline static std::string DefaultFormat = "yyyy.MM.dd hh:mm:ss.fffffff";
+	public:
+        const inline static std::string DefaultFormat = "yyyy.MM.dd hh:mm:ss.fffffff";
+        operator DateTime() const { return DateTime((int64_t)this->Ticks, DateTimeKind.Utc); }
 
-        public: std::uint64_t Ticks = 0;
+        // Timestamp (std::uint64_t ticks) : Timestamp(ticks) { }
+		//
+		// Can't see the difference between casting from a uint64_t and creating a new object;
+		// probably gonna remove that
 
-        public: Timestamp(std::uint64_t ticks) { Ticks = ticks; }
-
-        public: Timestamp(DateTime dateTime) : Timestamp((std::uint64_t)dateTime.ToUniversalTime().Ticks) { }
-
-        public: operator DateTime() const { return DateTime((std::int64_t)this->Ticks, DateTimeKind.Utc); }
-
-        public: Timestamp(std::uint64_t ticks) : Timestamp(ticks) { }
-
-        public: operator ulong() const { return this->Ticks; }
-
-        public: override std::string ToString() { return ((DateTime)this).ToString(DefaultFormat); }
-
-        public: bool operator ==(const Timestamp &other) const { return Ticks == other.Ticks; }
-
-        public: override std::int32_t GetHashCode() { return Ticks.GetHashCode(); }
+        operator uint64_t() const { return this->Ticks; } // I REALLY want to rename this to sth like
+													   // operator uint64_t, but the compatibility may
+													   // be ruined
+        bool operator ==(const Timestamp &other) const { return Ticks == other.getTicks; }
+        bool operator !=(const Timestamp &other) const { return Ticks != other.getTicks; }
+        override std::string ToString() { return ((DateTime)this).ToString(DefaultFormat); }
+        override int32_t GetHashCode() { return Ticks.GetHashCode(); } // The std::hash method
+																			// is available in
+																			// the <functional> header;
+																			// shall we include it?
+		uint64_t getTicks() const { return this->Ticks; }
+	private:
+        uint64_t Ticks;
     };
+
+	Timestamp::Timestamp(const uint64_t &ticks) : Ticks(ticks) { }
+
+    Timestamp::Timestamp(const DateTime &dateTime) : Timestamp((uint64_t)dateTime.ToUniversalTime().Ticks) { }
 }
