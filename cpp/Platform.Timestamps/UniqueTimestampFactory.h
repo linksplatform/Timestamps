790c751dd276b4c49ada7f5e86ccc43831163267
Stepan Ilyin <norwend@tuta.io>
2021-05-04 01:36:31 +0300

    stylistic changes and changed auto to uint64_t

--- cpp/Platform.Timestamps/UniqueTimestampFactory.h
+++ cpp/Platform.Timestamps/UniqueTimestampFactory.h
@@ -2,11 +2,12 @@
 {
     class UniqueTimestampFactory : public IFactory<Timestamp>
     {
-        private: std::uint64_t _lastTicks = 0;
-
-        public: Timestamp Create()
+    private: 
+        std::uint64_t _lastTicks = 0;
+    public: 
+        Timestamp Create()
         {
-            auto utcTicks = (std::uint64_t)DateTime.UtcNow.Ticks;
+            uint64_t utcTicks = (std::uint64_t)DateTime.UtcNow.Ticks;
             _lastTicks = utcTicks > _lastTicks ? utcTicks : _lastTicks + 1;
             return this->Timestamp(_lastTicks);
         }
