e25cf44236b2f8352852bf550910b346c1cc9ccd
Stepan Ilyin <norwend@tuta.io>
2021-05-10 18:42:15 +0300

    get_hash_code()

--- cpp/Platform.Timestamps/Timestamp.h
+++ cpp/Platform.Timestamps/Timestamp.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <functional>
 #include "CommonEraClock.h"
 
 namespace Platform::Timestamps
@@ -13,9 +14,8 @@ namespace Platform::Timestamps
 	    Timestamp(const uint64_t &ticks) : ticks_(ticks) { }
         Timestamp(const std::chrono::time_point<CommonEraClock> &tp) : ticks_(tp.time_since_epoch().count()) { }
         Timestamp() : ticks_(0) { }
-	    const std::string default_format = "%Y.%m.%d %H:%M:%S.";
         bool operator ==(const Timestamp &other) const { return ticks_ == other.get_ticks(); }
-		uint64_t get_ticks() const { return this->ticks_; }
+		uint64_t get_ticks() const noexcept { return this->ticks_; }
         std::string to_string()
         {
         	std::time_t timer = CommonEraClock::to_time_t(CommonEraClock::from_ticks(ticks_));
@@ -24,8 +24,21 @@ namespace Platform::Timestamps
         	string_stream << std::put_time(std::gmtime(&timer), default_format.c_str()) << temp.substr(temp.length() - 7);
         	return string_stream.str();
         }
-        // uint32_t GetHashCode() { return Ticks.GetHashCode(); }
 	private:
         uint64_t ticks_;
+	    const std::string default_format = "%Y.%m.%d %H:%M:%S.";
+
     };
+}
+
+namespace std
+{
+	template<>
+	struct hash<Platform::Timestamps::Timestamp>
+	{
+		size_t operator()(const Platform::Timestamps::Timestamp& timestamp) noexcept
+		{
+			return std::hash<uint64_t>{}(timestamp.get_ticks());
+		}
+	};
 }
