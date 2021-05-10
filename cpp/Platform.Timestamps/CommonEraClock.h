36a38b713b09bd607fd8448dec0d1dad5bfcb057
Stepan Ilyin <norwend@tuta.io>
2021-05-11 00:38:03 +0300

    stylistic changes

--- cpp/Platform.Timestamps/CommonEraClock.h
+++ cpp/Platform.Timestamps/CommonEraClock.h
@@ -4,55 +4,58 @@
 
 struct CommonEraClock
 {
-	static constexpr uint64_t ticks_after_ad = 621355968000000000;
-	using ticks = std::chrono::duration<uint64_t, std::ratio_multiply<std::nano, std::ratio<100> > >;
-	using duration = ticks;
-	using rep = duration::rep;
-	using period = duration::period;
-	using time_point = std::chrono::time_point<CommonEraClock>;
-	static const bool is_steady = false;
+    static constexpr uint64_t ticks_after_ad = 621355968000000000;
+    using ticks = std::chrono::duration<uint64_t, std::ratio_multiply<std::nano, std::ratio<100> > >;
+    using duration = ticks;
+    using rep = duration::rep;
+    using period = duration::period;
+    using time_point = std::chrono::time_point<CommonEraClock>;
+    static const bool is_steady = false;
 
-	static duration time_since_epoch()
-	{
-		return std::chrono::duration_cast<duration>
-		        (std::chrono::system_clock::now().time_since_epoch()) + duration(ticks_after_ad);
-	}
+        static duration time_since_epoch()
+        {
+            return std::chrono::duration_cast<duration>
+                (std::chrono::system_clock::now().time_since_epoch()) + duration(ticks_after_ad);
+        }
 
-	static time_point now() noexcept
-	{
-		return time_point(time_since_epoch());
-	}
+        static time_point now() noexcept
+        {
+            return time_point(time_since_epoch());
+        }
 
-	static std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds> to_sys(const time_point& tp) noexcept
-	{
-		return std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>
-		        (std::chrono::duration_cast<std::chrono::seconds>(tp.time_since_epoch())
-		                - std::chrono::duration_cast<std::chrono::seconds>(duration(ticks_after_ad)));
-	}
+        static std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>
+            to_sys(const time_point& common_time_point) noexcept
+        {
+            return std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>
+                    (std::chrono::duration_cast<std::chrono::seconds>(common_time_point.time_since_epoch())
+                            - std::chrono::duration_cast<std::chrono::seconds>(duration(ticks_after_ad)));
+        }
 
-	static time_point from_sys(const std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>& tp) noexcept
-	{
-		return time_point(std::chrono::duration_cast<duration>(tp.time_since_epoch()) + duration(ticks_after_ad));
-	}
+        static time_point from_sys(const std::chrono::time_point
+            <std::chrono::system_clock, std::chrono::seconds>& sys_time_point) noexcept
+        {
+            return time_point(std::chrono::duration_cast<duration>(sys_time_point.time_since_epoch())
+                + duration(ticks_after_ad));
+        }
 
-	static uint64_t to_ticks(const time_point& tp) noexcept
-	{
-		return tp.time_since_epoch().count();
-	}
+        static uint64_t to_ticks(const time_point& common_time_point) noexcept
+        {
+            return common_time_point.time_since_epoch().count();
+        }
 
-	static time_point from_ticks(const uint64_t& tick_number) noexcept
-	{
-		return time_point(duration(tick_number));
-	}
+        static time_point from_ticks(const uint64_t& tick_number) noexcept
+        {
+            return time_point(duration(tick_number));
+        }
 
-	static std::time_t to_time_t(const time_point& tp) noexcept
-	{
-		return std::chrono::system_clock::to_time_t(to_sys(tp));
-	}
+        static std::time_t to_time_t(const time_point& common_time_point) noexcept
+        {
+            return std::chrono::system_clock::to_time_t(to_sys(common_time_point));
+        }
 
-	static time_point from_time_t(const std::time_t& t) noexcept
-	{
-		return std::chrono::time_point_cast<duration>
-		        (std::chrono::time_point<CommonEraClock, duration>(std::chrono::seconds(t)));
-	}
+        static time_point from_time_t(const std::time_t& time) noexcept
+        {
+            return std::chrono::time_point_cast<duration>
+                (std::chrono::time_point<CommonEraClock, duration>(std::chrono::seconds(time)));
+        }
 };
--- cpp/Platform.Timestamps/Timestamp.h
+++ cpp/Platform.Timestamps/Timestamp.h
@@ -11,52 +11,48 @@ namespace Platform::Timestamps
     class Timestamp
     {
 	public:
-	    Timestamp(const uint64_t &t) : _ticks(t) { }
+        Timestamp(const uint64_t &ticks) : _ticks(ticks) { }
         Timestamp(const std::chrono::time_point<CommonEraClock> &tp) : _ticks(tp.time_since_epoch().count()) { }
         Timestamp() : _ticks(0) { }
 
         bool operator ==(const Timestamp &other) const { return _ticks == other.Ticks(); }
 
-		uint64_t Ticks() const noexcept { return _ticks; }
-		uint64_t Ticks(const uint64_t& t)
-		{
-	    	_ticks = t;
-	    	return t;
-		}
+        uint64_t Ticks() const noexcept { return _ticks; }
+        void Ticks(const uint64_t& ticks)   { _ticks = ticks; }
 
         operator std::string() const
         {
-        	std::time_t timer = CommonEraClock::to_time_t(CommonEraClock::from_ticks(_ticks));
-        	std::string temp = std::to_string(_ticks);
-        	std::stringstream stringStream;
-        	stringStream << std::put_time(std::gmtime(&timer), _defaultFormat.c_str()) << temp.substr(temp.length() - 7);
-        	return stringStream.str();
+            std::time_t time = CommonEraClock::to_time_t(CommonEraClock::from_ticks(_ticks));
+            std::stringstream stringStream;
+        	stringStream << std::put_time(std::gmtime(&time), _defaultFormat.c_str())
+                << '.' << _ticks % 10000000;
+            return stringStream.str();
         }
 
         friend std::ostream& operator<<(std::ostream& out, const Timestamp& timestamp)
         {
-			return out << std::string(timestamp);
+            return out << std::string(timestamp);
         }
 
-	private:
+    private:
         uint64_t _ticks;
-	    const std::string _defaultFormat = "%Y.%m.%d %H:%M:%S.";
+        const std::string _defaultFormat = "%Y.%m.%d %H:%M:%S";
     };
 }
 
 namespace std
 {
-	std::string to_string(const Platform::Timestamps::Timestamp& timestamp) noexcept
-	{
-		return std::string(timestamp);
-	}
+    std::string to_string(const Platform::Timestamps::Timestamp& timestamp) noexcept
+    {
+        return std::string(timestamp);
+    }
 
-	template<>
-	struct hash<Platform::Timestamps::Timestamp>
-	{
-		size_t operator()(const Platform::Timestamps::Timestamp& timestamp) noexcept
-		{
-			return std::hash<uint64_t>{}(timestamp.Ticks());
-		}
-	};
+    template<>
+    struct hash<Platform::Timestamps::Timestamp>
+    {
+        size_t operator()(const Platform::Timestamps::Timestamp& timestamp) noexcept
+        {
+            return std::hash<uint64_t>{}(timestamp.Ticks());
+        }
+    };
 }
