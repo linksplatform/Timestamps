c348db60f921100fff7051dc030c9814a536e18e
Stepan Ilyin <norwend@tuta.io>
2021-05-10 15:31:52 +0300

    refactoring and moving the using keyword inside a class

--- cpp/Platform.Timestamps/CommonEraClock.h
+++ cpp/Platform.Timestamps/CommonEraClock.h
@@ -2,21 +2,20 @@
 
 #include <chrono>
 
-using hundred_nanoseconds = std::chrono::duration<uint64_t, std::ratio_multiply<std::nano, std::ratio<100> > >;
-static inline const uint64_t ticks_after_ad = 621355968000000000;
-
 struct CommonEraClock
 {
-	using duration = hundred_nanoseconds;
+	static inline const uint64_t ticks_after_ad = 621355968000000000;
+	using tick = std::chrono::duration<uint64_t, std::ratio_multiply<std::nano, std::ratio<100> > >;
+	using duration = tick;
 	using rep = duration::rep;
 	using period = duration::period;
 	using time_point = std::chrono::time_point<CommonEraClock>;
 	static const bool is_steady = false;
 
-	static hundred_nanoseconds time_since_epoch()
+	static tick time_since_epoch()
 	{
 		return std::chrono::duration_cast<duration>
-		        (std::chrono::system_clock::now().time_since_epoch()) + hundred_nanoseconds(ticks_after_ad);
+		        (std::chrono::system_clock::now().time_since_epoch()) + tick(ticks_after_ad);
 	}
 
 	static time_point now() noexcept
@@ -28,12 +27,12 @@ struct CommonEraClock
 	{
 		return std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>
 		        (std::chrono::duration_cast<std::chrono::seconds>(tp.time_since_epoch())
-		                - std::chrono::seconds(ticks_after_ad));
+		                - std::chrono::duration_cast<std::chrono::seconds>(tick(ticks_after_ad)));
 	}
 
 	static time_point from_sys(const std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>& tp) noexcept
 	{
-		return time_point(std::chrono::duration_cast<duration>(tp.time_since_epoch()) + hundred_nanoseconds(ticks_after_ad));
+		return time_point(std::chrono::duration_cast<duration>(tp.time_since_epoch()) + tick(ticks_after_ad));
 	}
 
 	static uint64_t to_ticks(const time_point& tp) noexcept
@@ -43,12 +42,12 @@ struct CommonEraClock
 
 	static time_point from_ticks(const uint64_t& tick_number) noexcept
 	{
-		return time_point(hundred_nanoseconds(tick_number));
+		return time_point(tick(tick_number));
 	}
 
 	static std::time_t to_time_t(const time_point& tp) noexcept
 	{
-		return std::time_t(std::chrono::duration_cast<std::chrono::seconds>(tp.time_since_epoch()).count());
+		return std::chrono::system_clock::to_time_t(to_sys(tp));
 	}
 
 	static time_point from_time_t(const std::time_t& t) noexcept
