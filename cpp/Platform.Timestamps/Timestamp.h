a106dda2872ee70b759a0ea0e1f67b8423f037b2
Stepan Ilyin <norwend@tuta.io>
2021-05-11 19:57:33 +0300

    clang-format autoformatting

--- cpp/Platform.Timestamps/Timestamp.h
+++ cpp/Platform.Timestamps/Timestamp.h
@@ -1,35 +1,55 @@
 ﻿#pragma once
 
-#include <string>
-#include <sstream>
-#include <iomanip>
-#include <functional>
 #include "CommonEraClock.h"
+#include <functional>
+#include <iomanip>
+#include <sstream>
+#include <string>
 
 namespace Platform::Timestamps
 {
     class Timestamp
     {
-	public:
+    public:
         static constexpr const char* DefaultFormat = "%Y.%m.%d %H:%M:%S";
         static constexpr int TicksPerSecond = 10000000;
 
-        Timestamp(const uint64_t &ticks) : _ticks(ticks) { }
-        Timestamp(const std::chrono::time_point<CommonEraClock> &common_time_point)
-            : _ticks(common_time_point.time_since_epoch().count()) { }
-        Timestamp(const Timestamp& timestamp) : _ticks(timestamp.Ticks()) {}
-        Timestamp() : _ticks(0) { }
+        Timestamp(const uint64_t& ticks)
+            : _ticks(ticks)
+        {
+        }
+        Timestamp(const std::chrono::time_point<CommonEraClock>& common_time_point)
+            : _ticks(common_time_point.time_since_epoch().count())
+        {
+        }
+        Timestamp(const Timestamp& timestamp)
+            : _ticks(timestamp.Ticks())
+        {
+        }
+        Timestamp()
+            : _ticks(0)
+        {
+        }
 
-        uint64_t Ticks() const noexcept { return _ticks; }
-        void Ticks(const uint64_t& ticks)   { _ticks = ticks; }
+        uint64_t Ticks() const noexcept
+        {
+            return _ticks;
+        }
+        void Ticks(const uint64_t& ticks)
+        {
+            _ticks = ticks;
+        }
 
-        bool operator ==(const Timestamp &other) const { return _ticks == other.Ticks(); }
+        bool operator==(const Timestamp& other) const
+        {
+            return _ticks == other.Ticks();
+        }
         explicit operator std::string() const
         {
             std::time_t time = CommonEraClock::to_time_t(CommonEraClock::from_ticks(_ticks));
             std::stringstream stream;
-        	stream << std::put_time(std::gmtime(&time), DefaultFormat)
-                << '.' << _ticks % TicksPerSecond;
+            stream << std::put_time(std::gmtime(&time), DefaultFormat)
+                   << '.' << _ticks % TicksPerSecond;
             return stream.str();
         }
 
@@ -37,10 +57,11 @@ namespace Platform::Timestamps
         {
             return out << std::string(timestamp);
         }
+
     private:
         uint64_t _ticks;
     };
-}
+}// namespace Platform::Timestamps
 
 template<>
 struct std::hash<Platform::Timestamps::Timestamp>
