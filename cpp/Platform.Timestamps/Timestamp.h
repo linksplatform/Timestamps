7946a3290a99acc9145d27467094a76b9e7c267a
Stepan Ilyin <norwend@tuta.io>
2021-05-18 18:42:45 +0300

    stylistic changes

--- cpp/Platform.Timestamps/Timestamp.h
+++ cpp/Platform.Timestamps/Timestamp.h
@@ -10,28 +10,28 @@ namespace Platform::Timestamps
 {
     class Timestamp
     {
-    public: static constexpr const char* DefaultFormat = "%Y.%m.%d %H:%M:%S";
+        public: static constexpr const char* DefaultFormat = "%Y.%m.%d %H:%M:%S";
 
-    private: std::uint64_t _ticks;
+        private: std::uint64_t _ticks;
 
-    public: constexpr Timestamp(const std::uint64_t& ticks) noexcept
+        public: constexpr Timestamp(const std::uint64_t& ticks) noexcept
             : _ticks(ticks)
         {
         }
 
-    public: constexpr Timestamp(const std::chrono::time_point<common_era_clock>& common_time_point)
+        public: constexpr Timestamp(const std::chrono::time_point<common_era_clock>& common_time_point)
             : _ticks(common_time_point.time_since_epoch().count())
         {
         }
 
-    public: constexpr Timestamp(const Timestamp& timestamp)
+        public: constexpr Timestamp(const Timestamp& timestamp)
             : _ticks(timestamp.Ticks())
         {
         }
 
-    public: constexpr Timestamp() = default;
+        public: constexpr Timestamp() = default;
 
-    public: explicit operator std::string() const
+        public: explicit operator std::string() const
         {
             std::time_t time = common_era_clock::to_time_t(common_era_clock::from_ticks(_ticks));
             std::stringstream stream;
@@ -40,21 +40,21 @@ namespace Platform::Timestamps
             return stream.str();
         }
 
-    public: constexpr auto operator<=>(const Timestamp& other) const noexcept = default;
+        public: constexpr auto operator<=>(const Timestamp& other) const noexcept = default;
 
-    public: friend std::ostream& operator<<(std::ostream& out, const Timestamp& timestamp)
+        public: friend std::ostream& operator<<(std::ostream& out, const Timestamp& timestamp)
         {
             return out << static_cast<std::string>(timestamp);
         }
 
-    public: static constexpr std::uint64_t TicksPerSecond = 10'000'000;
+        public: static constexpr std::uint64_t TicksPerSecond = 10'000'000;
 
-    public: constexpr std::uint64_t Ticks() const noexcept
+        public: constexpr std::uint64_t Ticks() const noexcept
         {
             return _ticks;
         }
 
-    public: void Ticks(std::uint64_t ticks)
+        public: void Ticks(std::uint64_t ticks)
         {
             _ticks = ticks;
         }
