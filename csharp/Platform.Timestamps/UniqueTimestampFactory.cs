8bb9b87395ace05287b1ce118fad3d43c83d3e6b
FreePhoenix <freephoenix888@gmail.com>
2021-10-29 21:44:20 +0600

    Revert "Update XML Doc"
    This reverts commit 58291a4d42848da7dd28b66e434b7a4a32dddb72.

--- csharp/Platform.Timestamps/UniqueTimestampFactory.cs
+++ csharp/Platform.Timestamps/UniqueTimestampFactory.cs
@@ -10,6 +10,12 @@ namespace Platform.Timestamps
     /// </summary>
     public class UniqueTimestampFactory : IFactory<Timestamp>
     {
+        /// <summary>
+        /// <para>
+        /// The last ticks.
+        /// </para>
+        /// <para></para>
+        /// </summary>
         private ulong _lastTicks;
 
         /// <summary>
